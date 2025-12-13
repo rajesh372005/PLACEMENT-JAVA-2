34e28592ad63b9c5554b0ab8569bcb1b94dd4173
rajesh372005 <23btad043@kahedu.edu.in>
2025-12-13 13:55:24 +0530

    Add files via upload

---
+++ CHAR JAVA/Three.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Three {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            System.out.println("Alphabet");
+        } else {
+            System.out.println("No alphabet");
+        }
+    }
+}
